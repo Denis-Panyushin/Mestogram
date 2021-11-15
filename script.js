165d03bfe8a632aa910018157f8c37a001190380
Denis Panyushin <Panyushin.D@yandex.ru>
2021-11-16 01:14:29 +0300

    desktop version

---
+++ script.js
@@ -0,0 +1,34 @@
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__escape');
+let popupOpened = document.querySelector('.profile-info__edit-button');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_description');
+let name = document.querySelector('.profile-info__name');
+let description = document.querySelector('.profile-info__description');
+nameInput.value = document.querySelector('.profile-info__name').textContent;
+jobInput.value =  document.querySelector('.profile-info__description').textContent;
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+}
+
+popupOpened.addEventListener('click', popupOpen);
+
+function popupExit() {
+  popup.classList.remove('popup_opened');
+
+  nameInput.value = name.textContent;
+  jobInput.value = description.textContent;
+}
+
+popupClose.addEventListener('click', popupExit);
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  name.textContent = nameInput.value;
+  description.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
