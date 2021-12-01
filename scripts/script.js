8527b8954a0998b4ccd360e945e1b1b2fc686a60
Denis Panyushin <Panyushin.D@yandex.ru>
2021-12-01 20:17:23 +0300

    feat: форма картинок, форма добавления картинок, функция лайка

--- scripts/script.js
+++ scripts/script.js
@@ -27,7 +27,7 @@ const initialCards = [
 ];
 
 //Добавление карточек в html разметку
-const container = document.querySelector('.elements');
+const container = document.querySelector('.elements'); //Контейнер для карточек
 const templateEl = document.querySelector('#template-element');
 
 function render () {
@@ -55,9 +55,9 @@ function getEl (element) {
 
   const imgBtn = img
   img.addEventListener('click', function () {
-    const imagePopup = document.querySelector('.popup__image');
-    const popupPicture = document.querySelector('.popup__pic')
-    const popupDescription = document.querySelector('.popup__description');
+    const imagePopup = document.querySelector('.popup__image'); //Попап изображения
+    const popupPicture = document.querySelector('.popup__pic'); //Картинка карточки для попапа с изображением
+    const popupDescription = document.querySelector('.popup__description'); //Название места изображения
     popupDescription.textContent = element.name;
     popupPicture.setAttribute('src', element.link);
     openPopup(imagePopup);
@@ -91,30 +91,28 @@ function likeEl (evt) {
 }
 
 //Формы для редактирования и добавление карточек
-const popup = document.querySelector('.popup');
-const formElement = document.querySelector('.popup__container');
-const nameInput = document.querySelector('.popup__text_type_name');
-const jobInput = document.querySelector('.popup__text_type_description');
-const infoName = document.querySelector('.profile-info__name');
-const description = document.querySelector('.profile-info__description');
-const editPopup = document.querySelector('.popup__edit-profile');
-const addPopup = document.querySelector('.popup__add');
-const openAddPopupBtn = document.querySelector('.profile__add-button')
-const openEditPopupBtn = document.querySelector('.profile-info__edit-button');
-const escapeAddPopupBtn = addPopup.querySelector('.popup__escape');
-const escapeEditPopupBtn = editPopup.querySelector('.popup__escape');
-const mestoNameInput = document.querySelector('.popup__text_type_location');
-const mestoLinkInput = document.querySelector('.popup__text_type_img-link');
-const form = document.getElementById('form');
+const popup = document.querySelector('.popup'); //Блок попап
+const nameInput = document.querySelector('.popup__text_type_name'); //Поле Имя
+const jobInput = document.querySelector('.popup__text_type_description'); //Поле профессии
+const infoName = document.querySelector('.profile-info__name'); //Элемент имя
+const description = document.querySelector('.profile-info__description'); //Элемент професиия
+const editPopup = document.querySelector('.popup__edit-profile'); //Попап редактирования страницы
+const addPopup = document.querySelector('.popup__add'); //Попап добавления карточки
+const openAddPopupBtn = document.querySelector('.profile__add-button'); //Кнопка открытия попапа добавления карточки
+const openEditPopupBtn = document.querySelector('.profile-info__edit-button'); //Кнопка открытия попапа редактирования страницы
+const escapeAddPopupBtn = addPopup.querySelector('.popup__escape'); //Кнопка закрытия попапа добавления карточки
+const escapeEditPopupBtn = editPopup.querySelector('.popup__escape'); // Кнопка закрытия попапа редактирования
+const mestoNameInput = document.querySelector('.popup__text_type_location'); //Поле места изображения
+const mestoLinkInput = document.querySelector('.popup__text_type_img-link'); //Поле ссылки на изображение
 
 //Функция откртия
 function openPopup () {
   popup.classList.add('popup_opened');
 };
 
-const imagePopup = document.querySelector('.popup__image');
 //Функция закрытия
 function exitPopup () {
+  const imagePopup = document.querySelector('.popup__image');
   if (popup.classList.contains('popup_opened')) {
       popup.classList.remove('popup_opened');
       addPopup.classList.remove('popup_opened');
@@ -137,6 +135,7 @@ escapeAddPopupBtn.addEventListener('click', function () {
   exitPopup ();
 });
 
+//Функция добавления карточки
 function formSubmitHandlerAdd (evt) {
   evt.preventDefault();
   //Значения полей которые надо сохранить
@@ -145,7 +144,7 @@ function formSubmitHandlerAdd (evt) {
   const listItem = getEl({name: inputMesto, link: inputLink});
   container.prepend(listItem);
 
-  exitPopup (addPopup);
+  exitPopup ();
 }
 
 addPopup.addEventListener('submit', formSubmitHandlerAdd);
@@ -164,13 +163,14 @@ escapeEditPopupBtn.addEventListener('click', function () {
   exitPopup ();
 });
 
+//Функция редактирования карточки
 function formSubmitHandlerEdit (evt) {
   evt.preventDefault();
   //Значения полей которые надо сохранить
   infoName.textContent = nameInput.value;
   description.textContent = jobInput.value;
 
-  exitPopup(editPopup);
+  exitPopup();
 }
 
 editPopup.addEventListener('submit', formSubmitHandlerEdit);
